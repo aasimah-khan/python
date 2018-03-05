1fbac049ecefad793dfd4867a220c8f2cb7510cb
aasimah-khan <aasimahakhtarkhan786@gmail.com>
2018-03-05 12:24:40 +0530

    First Commit

---
+++ student.py
@@ -0,0 +1,18 @@
+n=int(raw_input("please enter no of students:"))
+student_data=['student_name','student_roll_no','student_class']
+for i in range(0,n):
+	student_name=raw_input('enter name of student:')
+	print student_name
+
+	student_roll_no=raw_input('enter the roll number of student:')
+	print student_roll_no
+	student_class=raw_input('enter the class of student:')
+	print student_class
+	dict={'name':student_name,'roll no':student_roll_no,'class':student_class}
+	print "dict['name']:",dict['name']
+	print "dict['roll no']:",dict['roll no']
+	print "dict['class']:",dict['class']
+n=int(raw_input("enter the name to be deleted"))
+student_info=['student_name']
+del student_info('student
+	
